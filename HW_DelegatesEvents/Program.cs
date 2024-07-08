4a7ec7907ad2164dfc274aaa1f0010284c1470aa
KennyDew <KennyDew@DESKTOP-1566CUE>
2024-07-09 00:52:02 +0500

    Add Subscriber.cs Вынес класс в отдельный файл

--- HW_DelegatesEvents/Program.cs
+++ HW_DelegatesEvents/Program.cs
@@ -25,32 +25,4 @@
             Console.WriteLine($"Max file: {maxFilePath}");
         }
     }
-
-
-    public class Subscriber
-    {
-        private string StopFileName { get; set; }
-
-        public Subscriber(string stopFileName)
-        {
-            StopFileName = stopFileName;
-        }
-
-        public void Onrecieved(object? sender, EventArgs e)
-        {
-            if (e is FileArgs fileArgs)
-            {
-                Console.WriteLine($"Recieved founded file: {fileArgs.FilePath}");
-                if (StopFileName != null && sender is FileSearcher fileSearcher && StopFileName == fileArgs.FilePath)
-                {
-                    fileSearcher.StopSearch = true;
-                }
-            }
-            else
-            {
-                Console.WriteLine($"Recieved founded file.");
-            }
-            
-        }
-    }
 }
---
+++ HW_DelegatesEvents/Subscriber.cs
@@ -0,0 +1,29 @@
+﻿namespace HW_DelegatesEvents
+{
+    public class Subscriber
+    {
+        private string StopFileName { get; set; }
+
+        public Subscriber(string stopFileName)
+        {
+            StopFileName = stopFileName;
+        }
+
+        public void Onrecieved(object? sender, EventArgs e)
+        {
+            if (e is FileArgs fileArgs)
+            {
+                Console.WriteLine($"Recieved founded file: {fileArgs.FilePath}");
+                if (StopFileName != null && sender is FileSearcher fileSearcher && StopFileName == fileArgs.FilePath)
+                {
+                    fileSearcher.StopSearch = true;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Recieved founded file.");
+            }
+
+        }
+    }
+}
